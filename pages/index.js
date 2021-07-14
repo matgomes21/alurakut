4a2d6a00b91c98119dbad32cb878899d70dc6cc8
Mateus Gomes <mateusgomes.nascimento21@gmail.com>
2021-07-13 23:19:51 -0300

    feat: create components

--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,22 @@
 import React from 'react';
-import styled from 'styled-components';
+import MainGrid from '../src/components/MainGrid';
+import Box from '../src/components/Box';
 
-const Box = styled.div`
-  background-color: #ffffff;
-  border-radius: 8px;
-`;
-
-const MainGrid = styled.main`
-  width: 100%;
-  grid-gap: 10px;
-  margin-left: auto;
-  margin-right: auto;
-  max-width: 500px;
-  padding: 16px;
-
-  .profileArea {
-    display: none;
-    @media(min-width: 860px) {
-      display: block;
-    }
-  }
-  @media(min-width: 860px) {
-    max-width: 1110px;
-    display: grid;
-    grid-template-areas:
-      "profileArea welcomeArea profileRelationsArea";
-    grid-template-columns: 160px 1fr 312px;
-  }
-`;
+function ProfileSideBar(props) {
+  return (
+    <Box>
+      <img src={`https://github.com/${props.githubUser}.png`} alt="User Profile" style={{ borderRadius: '8px'}} />
+    </Box>
+);
+}
 
 export default function Home() {
+const githubUser = 'matgomes21';
+
   return (
     <MainGrid>
       <div className="profileArea" style={{gridArea: 'profileArea'}}>
-        <Box>
-          Imagem
-        </Box>
+        <ProfileSideBar githubUser={githubUser} />
       </div>
       <div className="welcomeArea" style={{gridArea: 'welcomeArea'}}>
         <Box>
