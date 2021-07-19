56dd071bbe75c1aaacb6e74f69ee58e850ea1350
Mateus Gomes <mateusgomes.nascimento21@gmail.com>
2021-07-18 22:53:32 -0300

    feat: add community creation

--- pages/index.js
+++ pages/index.js
@@ -24,14 +24,16 @@ function ProfileSideBar(props) {
 }
 
 export default function Home() {
-const githubUser = 'matgomes21';
-const favoritePeople = [
-  'Matheusafonsouza',
-  'viniciussaturnino',
-  'matheusyanmonteiro',
-  'thiagompc',
-  'twistershark'
-];
+  const [communities, setCommunities] = React.useState(['Odeio segunda-feira']);
+
+  const githubUser = 'matgomes21';
+  const favoritePeople = [
+    'Matheusafonsouza',
+    'viniciussaturnino',
+    'matheusyanmonteiro',
+    'thiagompc',
+    'twistershark'
+  ];
 
   return (
     <>
@@ -48,8 +50,49 @@ const favoritePeople = [
 
             <OrkutNostalgicIconSet />
           </Box>
+
+          <Box>
+            <h2>O que você deseja fazer?</h2>
+            <form onSubmit={function handleCreateCommunity(e) {
+              e.preventDefault();
+              setCommunities([...communities, 'Odeio segunda-feira']);
+            }}>
+              <div>
+                <input
+                  placeholder="Qual vai ser o nome da sua comunidade?"
+                  name="title" 
+                  aria-label="Qual vai ser o nome da sua comunidade?"
+                  type="text"
+                />
+              </div>
+              <div>
+                <input
+                  placeholder="Coloque uma URL para ser usada de capa"
+                  name="image" 
+                  aria-label="Coloque uma URL para ser usada de capa"
+                  type="text"
+                />
+              </div>
+
+              <button type="submit" >
+                Criar comunidade
+              </button>
+            </form>
+          </Box>
         </div>
         <div className="profileRelationsArea" style={{gridArea: 'profileRelationsArea'}}>
+          <ProfileRelationsBoxWrapper>
+            <ul>
+              {communities.map((person)=>(
+                <li>
+                  <a href={`/users/${person}`} key={person}>
+                    <img src="https://placehold.it/300x300" alt={`${person} Profile`} />
+                    <span>{person}</span>
+                  </a>
+                </li>
+                ))}
+            </ul>
+          </ProfileRelationsBoxWrapper>
           <ProfileRelationsBoxWrapper>
             <h2 className="smallTitle">
               Pessoas ({favoritePeople.length})
